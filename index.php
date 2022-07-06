8863a48c7b3ae666e726c6ad026b88a90689e79d
Matt Brooks <mbrooks34@gsu.edu>
2022-07-06 09:26:07 -0400

    Shell index.

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>DataCite Bulk DOI Creator</title>
+
+   <link rel="stylesheet" href="css/bootstrap.min.css">
+
+   <script src="js/jquery.slim.min.js"></script>
+   <script src="js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+   <div class="container">
+      <h1>DataCite Bulk DOI Creator</h1>
+   </div>
+</body>
+</html>
