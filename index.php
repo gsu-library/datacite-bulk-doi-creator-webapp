c8cf621d079e41a1f8cddc58503193a969742c46
Matt Brooks <mbrooks34@gsu.edu>
2022-07-12 16:22:10 -0400

    Add example side area.

--- index.php
+++ index.php
@@ -16,15 +16,46 @@
       <h1 class="my-4">DataCite Bulk DOI Creator</h1>
 
       <div class="row">
-         <div class="col-lg">
-            <form action="." method="post">
+         <div class="col-lg-8">
+            <h2>Something</h2>
+            <form action="submit.php" method="post" enctype="multipart/form-data">
                <div class="form-group">
                   <label for="fileUpload">Upload File</label>
-                  <input type="file" id="fileUpload" class="form-control-file">
+                  <input type="file" id="fileUpload" class="form-control-file" name="fileUpload" accept=".csv">
                </div>
 
-               <button type="submit" class="btn btn-primary">Submit</button>
+               <button type="submit" class="btn btn-primary" name="submit">Submit</button>
             </form>
+         </div>
+
+         <div class="col-lg-4">
+            <h2><span class="text-muted">Upload Reports</span></h2>
+
+            <ul class="list-group mb-3">
+               <li class="list-group-item d-flex justify-content-between lh-condensed">
+                  <a href="#">file a.csv</a>
+               </li>
+               <li class="list-group-item d-flex justify-content-between lh-condensed">
+                  <a href="#">bio science.csv</a>
+               </li>
+               <li class="list-group-item d-flex justify-content-between lh-condensed">
+                  <a href="#">third file.csv</a>
+               </li>
+            </ul>
+
+
+            <h2><span class="text-muted">Last Uploaded CSVs</span></h2>
+            <ul class="list-group mb-3">
+               <li class="list-group-item d-flex justify-content-between lh-condensed">
+                  <a href="#">upload_report.20220710-111111.csv</a>
+               </li>
+               <li class="list-group-item d-flex justify-content-between lh-condensed">
+                  <a href="#">upload_report.20220621-091825.csv</a>
+               </li>
+               <li class="list-group-item d-flex justify-content-between lh-condensed">
+                  <a href="#">upload_report.20220603-232246.csv</a>
+               </li>
+            </ul>
          </div>
       </div>
    </div>
