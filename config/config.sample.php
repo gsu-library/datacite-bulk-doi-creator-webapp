4b4ae919033fb9350474a0a48b010e9fc4e3761c
Matt Brooks <mbrooks34@gsu.edu>
2023-02-08 16:27:26 -0500

    Add more configuration parameters for orcid.

--- config/config.sample.php
+++ config/config.sample.php
@@ -2,7 +2,8 @@
 /**
  * Configuration.
  *
- * @var array  [url, doiPrefix, username, password, maxSubmittedFiles, maxReportedFiles, maxUploadSize]
+ * @var array  [url, doiPrefix, username, password, maxSubmittedFiles, maxReportedFiles,
+ *              maxUploadSize, orcidTokenUrl, orcidApiUrl, orcidClientId, orcidSecret]
  */
 const CONFIG = [
    'url' => 'https://api.datacite.org/dois',
@@ -11,5 +12,9 @@ const CONFIG = [
    'password' => '',
    'maxSubmittedFiles' => 20,
    'maxReportFiles' => 20,
-   'maxUploadSize' => 10240
+   'maxUploadSize' => 10240,
+   'orcidTokenUrl' => 'https://orcid.org/oauth/token',
+   'orcidApiUrl' => 'https://pub.orcid.org/',
+   'orcidClientId' => '',
+   'orcidSecret' => ''
 ];
